55c55fffde0625ba3b39f13c40967dd025f76e58
Hans Van Slooten <HansVanSlooten@twins.com>
2025-08-23 12:23:38 -0500

    this was just test code

--- lisp-kernel/arch/arm/aarch64/arm-spentry64.s
+++ lisp-kernel/arch/arm/aarch64/arm-spentry64.s
@@ -18,28 +18,5 @@
 	_beginfile
 	.p2align 3
         
-_spentry(bind)
-	__(ldr imm1,[arg_y,#symbol.binding_index])
-	__(ldr imm0,[rcontext,#tcr.tlb_limit])
-	__(cmp imm0,imm1)
-        __(bhi 1f)
-	__(uuo_tlb_too_small(c_al,imm1))
-1:              
-	__(cmp imm1,#0)
-	__(ldr imm2,[rcontext,#tcr.tlb_pointer])
-	__(ldr imm0,[rcontext,#tcr.db_link])
-	__(ldr temp1,[imm2,imm1])
-	__(beq 9f)
-	__(vpush1(temp1))
-	__(vpush1(imm1))
-	__(vpush1(imm0))
-	__(str arg_z,[imm2,imm1])
-	__(str vsp,[rcontext,#tcr.db_link])
-	__(bx lr)
-9:
-	__(mov arg_z,arg_y)
-	__(mov arg_y,#XSYMNOBIND)
-	__(set_nargs(2))
-	__(b _SPksignalerr)
-        
+
         _endfile
