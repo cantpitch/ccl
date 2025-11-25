32d1b6a2d70dbb9739e24f348eee034f39b82bcd
Hans Van Slooten <cantpitch@protonmail.com>
2025-11-25 16:37:11 -0600

    Hex Fiend Template for CCL Image File

---
+++ lisp-kernel/00devdocs/CCL Image.tcl
@@ -0,0 +1,130 @@
+#
+# Hex Fiend Template for CCL Image File
+# 
+# Place in:
+# '/Users/<username>/Library/Application Support/com.ridiculousfish.HexFiend/Templates'
+#
+
+set section_names(0) ""
+set section_sizes(0) 0
+
+proc SectionHeader {index} {
+    global section_names
+    global section_sizes
+
+    set code_pos [pos]
+    set code [expr [uint64] >> 3]
+    
+    switch $code {
+        0 {set name "AREA_VOID"}
+        1 {set name "AREA_CSTACK"}
+        2 {set name "AREA_VSTACK"}
+        3 {set name "AREA_TSTACK"}
+        4 {set name "AREA_READONLY"}
+        5 {set name "AREA_WATCHED"}
+        6 {set name "AREA_STATIC_CONS"}
+        7 {set name "AREA_MANAGED_STATIC"}
+        8 {set name "AREA_STATIC"}
+        9 {set name "AREA_DYNAMIC"}
+        default {set name "Unknown"}
+    }
+
+    set section_names($index) $name
+
+    section -collapsed "$name Header" {
+        entry "Code" $code 8 $code_pos
+        uint64 -hex "Area"
+        set mem_size [uint64]
+        set section_sizes($index) $mem_size
+        entry "Memory Size" $mem_size 8 [expr $code_pos + 16]
+        uint64 "Static dnodes"
+    }
+}
+
+proc Section {name size} {
+    
+    set round_size [expr {$size + 0xFFF} & ~0xFFF]
+    section "$name Data" {
+        if {$size > 0} {
+            bytes $round_size "Data"
+        }
+    }
+}
+
+section "Footer" {
+    set magic_loc [expr [len] - 16]
+    goto $magic_loc
+
+    requires $magic_loc "6E 65 70 4F 49 4C 43 4D 65 67 61 6D"
+
+    set sig [ascii 12]
+    entry "Signature" $sig 12 $magic_loc
+    set delta [int32]
+    entry "Delta" $delta 4 [expr $magic_loc + 12]
+}
+
+section "Header" {
+    move $delta
+
+    ascii 16 "Signature"
+    unixtime32 "Timestamp"
+    uint32 -hex "Canonical Image Base 32"
+    uint32 -hex "Actual Image Base 32"
+    set sections [uint32]
+    entry "Sections" $sections
+    uint32 "ABI Version"
+    set section_data_offset [int32]
+    entry "Section Data Offset High" $section_data_offset
+    set section_data_offset_low [uint32]
+    entry "Section Data Offset Low" $section_data_offset_low
+    set section_data_offset [expr {$section_data_offset << 32} | $section_data_offset_low]
+    entry "(Section Data Offset)" $section_data_offset
+    
+    set curr_pos [pos]
+    set flags [uint32]
+    section Flags {
+        switch [expr $flags & 0x7] {
+            0 {set os "VxWorks"}
+            1 {set os "Linux"}
+            2 {set os "Solaris"}
+            3 {set os "Darwin"}
+            4 {set os "FreeBSD"}
+            5 {set os "Windows"}
+            6 {set os "Android"}
+            default {set os "Unknown" 4 $curr_pos}
+        }
+        entry "OS" $os 4 $curr_pos
+        
+        switch [expr $flags & 0x18 >> 2] {
+            0 {set cpu "PowerPC"}
+            1 {set cpu "SPARC"}
+            2 {set cpu "x86"}
+            3 {set cpu "Arm"}
+            default {set cpu "Unknown"}
+        }
+        entry "CPU" $cpu 4 $curr_pos
+
+        set word_size [expr $flags & 0x40 >> 6]
+        switch $word_size {
+            0 {set ws "32-bit"}
+            1 {set ws "64-bit"}
+            default {set ws "Unknown"}
+        }
+        entry "Word Size" $ws 4 $curr_pos
+    }
+    if {$word_size == 1} {
+        uint64 -hex "Canonical Image Base 64"
+        uint64 -hex "Actual Image Base 64"
+    }
+}
+
+for {set i 0} {$i < $sections} {incr i} {
+    SectionHeader $i
+}
+
+move $section_data_offset
+
+
+for {set i 0} {$i < $sections} {incr i} {
+    Section $section_names($i) $section_sizes($i)
+}
